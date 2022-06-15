af25915f53667b9fd24eecd408fac2563be27edc
fengkx <liangkx8237@gmail.com>
2022-06-15 16:48:23 +0800

    chore: compile css

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+const gulp = require("gulp");
+const debug = require("gulp-debug");
+const replace = require("gulp-replace");
+const sass = require("gulp-sass")(require("sass"));
+
+function compileSass() {
+  return gulp
+    .src("src/**/*.scss")
+    .pipe(sass().on("error", sass.logError))
+    .pipe(gulp.dest("lib"));
+}
+
+gulp.task("css:compile", compileSass);
+
+function replaceSassImport() {
+  return gulp
+    .src(["lib/**/*.js"])
+    .pipe(
+      replace(/^import\s+(?:'|")(.+?)(?:'|")/gm, (match, p1) => {
+        return match.replace(p1, p1.replace(".scss", ".css"));
+      })
+    )
+    .pipe(gulp.dest("lib"));
+}
+
+gulp.task("css:replace", replaceSassImport);
+
+exports.css = gulp.series("css:compile", "css:replace");
--- src/editor/index.tsx
+++ src/editor/index.tsx
@@ -40,7 +40,7 @@ import {
 import ControlBar from '../components/business/ControlBar'
 
 import 'draft-js/dist/Draft.css'
-import '../assets/scss/_kedao.scss'
+import '../assets/scss/kedao.scss'
 import {
   CallbackEditor,
   ControlItem,

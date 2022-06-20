81275309a11c12f896af217bf813e5fbb3a58701
banyudu <banyudu@gmail.com>
2022-06-20 16:45:43 +0800

    chore: fix development issue in build:watch mode

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 const gulp = require("gulp");
-const replace = require("gulp-replace");
+// const replace = require("gulp-replace");
 const sass = require("gulp-sass")(require("sass"));
 
 function compileSass() {
@@ -11,17 +11,19 @@ function compileSass() {
 
 gulp.task("css:compile", compileSass);
 
-function replaceSassImport() {
-  return gulp
-    .src(["lib/**/*.js"])
-    .pipe(
-      replace(/^import\s+(?:'|")(.+?)(?:'|")/gm, (match, p1) => {
-        return match.replace(p1, p1.replace(".scss", ".css"));
-      })
-    )
-    .pipe(gulp.dest("lib"));
-}
+// use ts-rename-import-plugin instead of gulp
+// function replaceSassImport() {
+//   return gulp
+//     .src(["lib/**/*.js"])
+//     .pipe(
+//       replace(/^import\s+(?:'|")(.+?)(?:'|")/gm, (match, p1) => {
+//         return match.replace(p1, p1.replace(".scss", ".css"));
+//       })
+//     )
+//     .pipe(gulp.dest("lib"));
+// }
 
-gulp.task("css:replace", replaceSassImport);
+// gulp.task("css:replace", replaceSassImport);
 
-exports.css = gulp.series("css:compile", "css:replace");
+// exports.css = gulp.series("css:compile", "css:replace");
+exports.css = gulp.series("css:compile");

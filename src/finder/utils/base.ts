1adb1affdb8c3b411fd5924c9ca39bae3ad5b9d2
banyudu <banyudu@gmail.com>
2022-07-28 21:14:37 +0800

    refactor: remove duplicate UniqueIndex function

--- src/finder/utils/base.ts
+++
@@ -1,9 +0,0 @@
-export const UniqueIndex = () => {
-  if (isNaN(window.__KEDAO_MM_UNIQUE_INDEX__)) {
-    window.__KEDAO_MM_UNIQUE_INDEX__ = 1
-  } else {
-    window.__KEDAO_MM_UNIQUE_INDEX__ += 1
-  }
-
-  return window.__KEDAO_MM_UNIQUE_INDEX__
-}

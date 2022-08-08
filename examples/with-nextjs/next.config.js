287488ebae59a5624e4ee2e56d58c73319b49e87
banyudu <banyudu@gmail.com>
2022-08-09 05:25:12 +0800

    fix: build errors

--- examples/with-nextjs/next.config.js
+++ examples/with-nextjs/next.config.js
@@ -1,5 +1,5 @@
 /** @type {import('next').NextConfig} */
-const withTM = require('next-transpile-modules')(['kedao']);
+const withTM = require('next-transpile-modules')(['kedao', 'draft-js']);
 module.exports = withTM({
   reactStrictMode: true,
 });
